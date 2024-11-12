08fa8674f29c473babcabda781deecf834ab810c
nimaxrad <92433720+nimaxrad@users.noreply.github.com>
2024-11-12 15:26:08 -0800

    Create refactor_foundation.py
    test function

---
+++ refactor_foundation.py
@@ -0,0 +1,9 @@
+#standard edition is re-installed within the same directory for test purposes
+
+import os
+import random
+import parakeet
+import math
+from os import system
+
+virtual_space_details.fetch(random.randint()).
